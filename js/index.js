6dfab952b2a21739dbf41c0833fc4b92d5c98e4b
Nolan Welch <bugygamer.mc@gmail.com>
2023-03-01 23:12:19 -0700

    Complete Lab

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const grabForm = document.getElementsByTagName('form')
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>(
+    document.addEventListener('submit', (event) => event.preventDefault())
+    
+    
+))
+
+
+// let saerchUsers = fetch('https://api.github.com/search/users?q=octocat' )
+//     .then ( (res)=> res.json())
+//     .then (data => console.log(data))
+
+// console.log (saerchUsers) 
+
+
+
+
+function submit (){
+    console.log (document.getElementsByTagName("input"))
+    
+}
+submit()
+
+//this is what I came up with for the mooment ill returing to this firday with some help , im summitng it and moveing on.
